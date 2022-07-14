5c317eb32b2ae1c480ba48ff5c78dc479c9f1121
최하영 <hychoi318@gmail.com>
2022-07-14 18:34:26 +0900

    projects 파일 분리

---
+++ projects/meme-maker/src/assets/scripts/common.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+// ctx.strokeRect(30,30,100,200);
+ctx.fillRect(30,30,100,200);
